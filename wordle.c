34d028ce9b6ffee6f2c6651b73a6912f1996d9a9
JBO <jbo@ipa.jbo.com>
2024-10-19 13:39:41 -0700

    working kind of

--- wordle.c
+++ wordle.c
@@ -1,13 +1,70 @@
+/************************************************************************
+*                                                                       *
+*  File:       wordle.c                                                 *
+*  Purpose:    Terminal-based game of Wordle.                           *
+*                                                                       *
+*************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <string.h>
 #include <stdbool.h>
+#include <limits.h>
 
+/************************************************************************
+*                                                                       *
+*  Constant:   WORD_LENGTH                                              *
+*  Purpose:    Defines the word length.                                 *
+*                                                                       *
+*************************************************************************/
 #define WORD_LENGTH (6)
+
+/************************************************************************
+*                                                                       *
+*  Constant:   MAX_WORDS                                                *
+*  Purpose:    Defines the maximum number of words.                     *
+*                                                                       *
+*************************************************************************/
 #define MAX_WORDS (10000)
+
+/************************************************************************
+*                                                                       *
+*  Constant:   MAX_DICTIONARY_SIZE                                      *
+*  Purpose:    Defines the maximum dictionary size, in bytes.           *
+*                                                                       *
+*************************************************************************/
 #define MAX_DICTIONARY_SIZE (WORD_LENGTH * MAX_WORDS)
+
+/************************************************************************
+*                                                                       *
+*  Constant:   ATTEMPTS_NUM                                             *
+*  Purpose:    Defines the number of attempts per round.                *
+*                                                                       *
+*************************************************************************/
 #define ATTEMPTS_NUM (5)
+
+/************************************************************************
+*                                                                       *
+*  Constant:   GREEN                                                    *
+*  Purpose:    Defines the color green.                                 *
+*                                                                       *
+*************************************************************************/
+#define GREEN ("\x1b[32m\x1b[1m")
+
+/************************************************************************
+*                                                                       *
+*  Constant:   YELLOW                                                   *
+*  Purpose:    Defines the color yellow.                                *
+*                                                                       *
+*************************************************************************/
+#define YELLOW ("\x1b[33m\x1b[1m")
+
+/************************************************************************
+*                                                                       *
+*  Constant:   RESET_COLOR                                              *
+*  Purpose:    Defines the string that resets the color.                *
+*                                                                       *
+*************************************************************************/
+#define RESET_COLOR ("\x1b[0m")
 
 /************************************************************************
 *                                                                       *
@@ -37,7 +94,142 @@ void
 print_logo(void)
 {
     (void)printf("\e[1;1H\e[2J");
-    (void)printf("\n\x1b[37m\x1b[1m  ██     ██   \x1b[31m\x1b[1m,d88b.d88b,\x1b[37m\x1b[1m  ██████  ██████  ██      ███████\n  ██     ██   \x1b[31m\x1b[1m88888888888\x1b[37m\x1b[1m  ██   ██ ██   ██ ██      ██     \n  ██  █  ██   \x1b[31m\x1b[1m`Y8888888Y\'\x1b[37m\x1b[1m  ██████  ██   ██ ██      █████  \n  ██ ███ ██     \x1b[31m\x1b[1m`Y888Y\'\x1b[37m\x1b[1m    ██   ██ ██   ██ ██      ██     \n   ███ ███        \x1b[31m\x1b[1m`Y\'\x1b[37m\x1b[1m      ██   ██ ██████  ███████ ███████\n\n             Level by Jonathan Bar Or (\"JBO\")\n                     @yo_yo_yo_jbo\n\x1b[0m-----------------------------------------------------------\n");
+    (void)printf("\n\x1b[37m\x1b[1m  ██     ██   \x1b[31m\x1b[1m,d88b.d88b,\x1b[37m\x1b[1m  ██████  ██████  ██      ███████\n  ██     ██   \x1b[31m\x1b[1m88888888888\x1b[37m\x1b[1m  ██   ██ ██   ██ ██      ██     \n  ██  █  ██   \x1b[31m\x1b[1m`Y8888888Y\'\x1b[37m\x1b[1m  ██████  ██   ██ ██      █████  \n  ██ ███ ██     \x1b[31m\x1b[1m`Y888Y\'\x1b[37m\x1b[1m    ██   ██ ██   ██ ██      ██     \n   ███ ███        \x1b[31m\x1b[1m`Y\'\x1b[37m\x1b[1m      ██   ██ ██████  ███████ ███████\n\n             Level by Jonathan Bar Or (\"JBO\")\n                     @yo_yo_yo_jbo\n\x1b[0m\n\n");
+}
+
+/************************************************************************
+*                                                                       *
+*  Function:   print_attempt                                            *
+*  Purpose:    Prints an attempt.                                       *
+*  Parameters: - attempt - the attempt, assumed to be a valid word.     *
+*              - word - the correct word, assumed to be a valid word.   *
+*  Returns:    True if the word and the attempt match, false otherwise. *
+*                                                                       *
+*************************************************************************/
+static
+bool
+print_attempt(
+    char* attempt,
+    char* word
+)
+{
+    char* colors[WORD_LENGTH] = { NULL };
+    size_t counter = 0;
+    int frequencies[256] = { 0 };
+    int hits = 0;
+
+    // Get letter frequencies
+    for (counter = 0; counter < WORD_LENGTH; counter++)
+    {
+        frequencies[word[counter]]++;
+    }
+
+    // Finds the correct letters and assigns green to them
+    for (counter = 0; counter < WORD_LENGTH; counter++)
+    {
+        if (word[counter] == attempt[counter])
+        {
+            colors[counter] = GREEN;
+            frequencies[word[counter]]--;
+            hits++;
+        }
+    }
+
+    // Finds misplaced letters
+    for (counter = 0; counter < WORD_LENGTH; counter++)
+    {
+        if ((NULL == colors[counter]) && (0 < frequencies[attempt[counter]]))
+        {
+            colors[counter] = YELLOW;
+            frequencies[attempt[counter]]--;
+        }
+    }
+
+    // Print the result
+    for (counter = 0; counter < WORD_LENGTH; counter++)
+    {
+        printf("%s%c%s", colors[counter] != NULL ? colors[counter] : "", attempt[counter], RESET_COLOR);
+    }
+
+    // Indicate success
+    return WORD_LENGTH == hits;
+}
+
+/************************************************************************
+*                                                                       *
+*  Function:   play_one_round                                           *
+*  Purpose:    Plays one round.                                         *
+*  Parameters: - dictionary - the dictionary.                           *
+*              - num_words - the number of words.                       *
+*  Returns:    True if the round ended well, false upon errors.         *
+*                                                                       *
+*************************************************************************/
+static
+bool
+play_one_round(
+    char* dictionary,
+    size_t num_words
+)
+{
+    bool result = false;
+    char curr_word[WORD_LENGTH + 1] = { 0 };
+    char curr_attempt[WORD_LENGTH + 1] = { 0 };
+    char past_attempts[ATTEMPTS_NUM][WORD_LENGTH + 1] = { 0 };
+    unsigned long attempts = 0;
+    unsigned long counter = 0;
+    bool won = false;
+    
+    // Choose a new word randomly
+    (void)strncpy(curr_word, dictionary + ((rand() % num_words) * WORD_LENGTH), sizeof(curr_word));
+
+    // Play all rounds
+    while (ATTEMPTS_NUM > attempts)
+    {
+        // Print game so far
+        print_logo();
+        for (counter = 0; counter < attempts; counter++)
+        {
+            (void)print_attempt(past_attempts[counter], curr_word);
+        }
+
+        // Get user input
+        (void)printf("Enter your current attempt: ");
+        (void)memset(curr_attempt, '\0', sizeof(curr_attempt));
+        if (NULL == fgets(curr_attempt, sizeof(curr_attempt), stdin))
+        {
+            write_error("Failed reading user input.");
+            goto cleanup;
+        }
+
+        // Check the attempt
+        if (print_attempt(curr_attempt, curr_word))
+        {
+            won = true;
+            break;
+        }
+
+        // Save the attempt as a past attempt
+        (void)strncpy(past_attempts[attempts], curr_attempt, sizeof(curr_attempt));
+        attempts++;
+    }
+
+    // Print solution
+    if (won)
+    {
+        printf("\nGreat job on guessing the word %s%s%s!\n", GREEN, curr_word, RESET_COLOR);
+    }
+    else
+    {
+        printf("\nYou failed guessing the word %s%s%s.\n", GREEN, curr_word, RESET_COLOR);
+    }
+
+    // Success
+    result = true;
+
+cleanup:
+
+    // Return result
+    return result;
 }
 
 /************************************************************************
@@ -55,32 +247,32 @@ play(
     size_t num_words
 )
 {
-    char curr_word[WORD_LENGTH + 1] = { 0 };
-    char curr_attempt[WORD_LENGTH + 1] = { 0 };
-    int attempts = 0;
-    bool won = false;
-
+    unsigned long rounds = 0;
+    unsigned long wins = 0;
+    int choice = 0;
+    bool quit = false; 
+ 
     // Play forever
-    for (;;)
+    while (!quit)
     {
-        // Choose a new word randomly
-        (void)strncpy(curr_word, dictionary + ((rand() % num_words) * WORD_LENGTH), sizeof(curr_word));
-
-        // Play all rounds
-        while (ATTEMPTS_NUM > attempts)
+        // Play a round
+        if (!play_one_round(dictionary, num_words))
         {
-            // Print game so far
-            print_logo();
+            goto cleanup;
+        }
 
-            // Get user input
-            (void)printf("Enter your current attempt: ");
-            (void)memset(curr_attempt, '\0', sizeof(curr_attempt));
-            if (NULL == fgets(curr_attempt, sizeof(curr_attempt), stdin))
+        // Optionally play again
+        printf("Play again [Y/N]: ");
+        do
+        {
+            choice = getchar();
+            if (('N' == choice) || ('n' == choice))
             {
-                write_error("Failed reading user input.");
-                goto cleanup;
+                quit = true;
+                break;
             }
         }
+        while (('Y' != choice) && ('y' != choice));
     }
 
 cleanup:
@@ -194,6 +386,13 @@ cleanup:
     return result;
 }
 
+/************************************************************************
+*                                                                       *
+*  Function:   main                                                     *
+*  Purpose:    Main routine.                                            *
+*  Returns:    Zero, always.                                            *
+*                                                                       *
+*************************************************************************/
 int
 main()
 {
